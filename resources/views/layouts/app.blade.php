3e44ab34846153a0e3cb14d47ee6f439b214599c
SebastianPal <spalewski@gmail.com>
2019-11-04 10:57:39 +0100

    Update profile func updated: - not creating new entries in DB if entry with profile_ID already exist - UserProfile update form protected again empty values

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Loyalyty') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,8 +18,224 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <meta charset="utf-8" />
+    <title>Metronic Admin Theme #6 | New User Profile | Account</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Preview page of Metronic Admin Theme #6 for user account page" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN LAYOUT FIRST STYLES -->
+    <link href="//fonts.googleapis.com/css?family=Oswald:400,300,700" rel="stylesheet" type="text/css" />
+    <!-- END LAYOUT FIRST STYLES -->
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <link href="../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link href="../assets/pages/css/profile.min.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="../assets/layouts/layout6/css/layout.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/layouts/layout6/css/custom.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" /> </head>
+
+
+
 </head>
 <body>
+
+
+
+<!-- BEGIN HEADER -->
+<header class="page-header">
+    <nav class="navbar" role="navigation">
+        <div class="container-fluid">
+            <div class="havbar-header">
+                <!-- BEGIN LOGO -->
+                <a id="index" class="navbar-brand" href="start.html">
+                    <img src="../assets/layouts/layout6/img/logo.png" alt="Logo"> </a>
+                <!-- END LOGO -->
+                <!-- BEGIN TOPBAR ACTIONS -->
+                <div class="topbar-actions">
+                    <!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
+                    <form class="search-form" action="extra_search.html" method="GET">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search here" name="query">
+                            <span class="input-group-btn">
+                                        <a href="javascript:;" class="btn md-skip submit">
+                                            <i class="fa fa-search"></i>
+                                        </a>
+                                    </span>
+                        </div>
+                    </form>
+                    <!-- END HEADER SEARCH BOX -->
+                    <!-- BEGIN GROUP NOTIFICATION -->
+                    <div class="btn-group-notification btn-group" id="header_notification_bar">
+                        <button type="button" class="btn md-skip dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                            <span class="badge">9</span>
+                        </button>
+                        <ul class="dropdown-menu-v2">
+                            <li class="external">
+                                <h3>
+                                    <span class="bold">12 pending</span> notifications</h3>
+                                <a href="#">view all</a>
+                            </li>
+                            <li>
+                                <ul class="dropdown-menu-list scroller" style="height: 250px; padding: 0;" data-handle-color="#637283">
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-success md-skip">
+                                                            <i class="fa fa-plus"></i>
+                                                        </span> New user registered. </span>
+                                            <span class="time">just now</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-danger md-skip">
+                                                            <i class="fa fa-bolt"></i>
+                                                        </span> Server #12 overloaded. </span>
+                                            <span class="time">3 mins</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-warning md-skip">
+                                                            <i class="fa fa-bell-o"></i>
+                                                        </span> Server #2 not responding. </span>
+                                            <span class="time">10 mins</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-info md-skip">
+                                                            <i class="fa fa-bullhorn"></i>
+                                                        </span> Application error. </span>
+                                            <span class="time">14 hrs</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-danger md-skip">
+                                                            <i class="fa fa-bolt"></i>
+                                                        </span> Database overloaded 68%. </span>
+                                            <span class="time">2 days</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-danger md-skip">
+                                                            <i class="fa fa-bolt"></i>
+                                                        </span> A user IP blocked. </span>
+                                            <span class="time">3 days</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-warning md-skip">
+                                                            <i class="fa fa-bell-o"></i>
+                                                        </span> Storage Server #4 not responding dfdfdfd. </span>
+                                            <span class="time">4 days</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-info md-skip">
+                                                            <i class="fa fa-bullhorn"></i>
+                                                        </span> System Error. </span>
+                                            <span class="time">5 days</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                                    <span class="details">
+                                                        <span class="label label-sm label-icon label-danger md-skip">
+                                                            <i class="fa fa-bolt"></i>
+                                                        </span> Storage server failed. </span>
+                                            <span class="time">9 days</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END GROUP NOTIFICATION -->
+                    <!-- BEGIN USER PROFILE -->
+                    <div class="btn-group-img btn-group">
+                        <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                            <img src="../assets/layouts/layout5/img/avatar1.jpg" alt=""> </button>
+                        <ul class="dropdown-menu-v2" role="menu">
+                            <li>
+                                <a href="page_user_profile_1.html">
+                                    <i class="icon-user"></i> My Profile
+                                    <span class="badge badge-danger">1</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="app_calendar.html">
+                                    <i class="icon-calendar"></i> My Calendar </a>
+                            </li>
+                            <li>
+                                <a href="app_inbox.html">
+                                    <i class="icon-envelope-open"></i> My Inbox
+                                    <span class="badge badge-danger"> 3 </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="app_todo_2.html">
+                                    <i class="icon-rocket"></i> My Tasks
+                                    <span class="badge badge-success"> 7 </span>
+                                </a>
+                            </li>
+                            <li class="divider"> </li>
+                            <li>
+                                <a href="page_user_lock_1.html">
+                                    <i class="icon-lock"></i> Lock Screen </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}"   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    <i class="icon-key"></i> Log Out </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END USER PROFILE -->
+                </div>
+                <!-- END TOPBAR ACTIONS -->
+            </div>
+        </div>
+        <!--/container-->
+    </nav>
+</header>
+<!-- END HEADER -->
+
+
+
+
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -76,5 +292,11 @@
             @yield('content')
         </main>
     </div>
+
+
+
+
+
+
 </body>
 </html>
--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
 <!DOCTYPE html>
 <!--
 Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.7
@@ -54,128 +57,6 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- END HEAD -->
 
 <body class="">
-<!-- BEGIN HEADER -->
-<header class="page-header">
-    <nav class="navbar" role="navigation">
-        <div class="container-fluid">
-            <div class="havbar-header">
-                <!-- BEGIN LOGO -->
-                <a id="index" class="navbar-brand" href="start.html">
-                    <img src="../assets/layouts/layout6/img/logo.png" alt="Logo"> </a>
-                <!-- END LOGO -->
-                <!-- BEGIN TOPBAR ACTIONS -->
-                <div class="topbar-actions">
-                    <!-- DOC: Apply "search-form-expanded" right after the "search-form" class to have half expanded search box -->
-                    <form class="search-form" action="extra_search.html" method="GET">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search here" name="query">
-                            <span class="input-group-btn">
-                                        <a href="javascript:;" class="btn md-skip submit">
-                                            <i class="fa fa-search"></i>
-                                        </a>
-                                    </span>
-                        </div>
-                    </form>
-                    <!-- END HEADER SEARCH BOX -->
-                    <!-- BEGIN GROUP NOTIFICATION -->
-                    <div class="btn-group-notification btn-group" id="header_notification_bar">
-                        <button type="button" class="btn md-skip dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                            <span class="badge">9</span>
-                        </button>
-                        <ul class="dropdown-menu-v2">
-                            <li class="external">
-                                <h3>
-                                    <span class="bold">12 pending</span> notifications</h3>
-                                <a href="#">view all</a>
-                            </li>
-                            <li>
-                                <ul class="dropdown-menu-list scroller" style="height: 250px; padding: 0;" data-handle-color="#637283">
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-success md-skip">
-                                                            <i class="fa fa-plus"></i>
-                                                        </span> New user registered. </span>
-                                            <span class="time">just now</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-danger md-skip">
-                                                            <i class="fa fa-bolt"></i>
-                                                        </span> Server #12 overloaded. </span>
-                                            <span class="time">3 mins</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-warning md-skip">
-                                                            <i class="fa fa-bell-o"></i>
-                                                        </span> Server #2 not responding. </span>
-                                            <span class="time">10 mins</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-info md-skip">
-                                                            <i class="fa fa-bullhorn"></i>
-                                                        </span> Application error. </span>
-                                            <span class="time">14 hrs</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-danger md-skip">
-                                                            <i class="fa fa-bolt"></i>
-                                                        </span> Database overloaded 68%. </span>
-                                            <span class="time">2 days</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-danger md-skip">
-                                                            <i class="fa fa-bolt"></i>
-                                                        </span> A user IP blocked. </span>
-                                            <span class="time">3 days</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-warning md-skip">
-                                                            <i class="fa fa-bell-o"></i>
-                                                        </span> Storage Server #4 not responding dfdfdfd. </span>
-                                            <span class="time">4 days</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-info md-skip">
-                                                            <i class="fa fa-bullhorn"></i>
-                                                        </span> System Error. </span>
-                                            <span class="time">5 days</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                                    <span class="details">
-                                                        <span class="label label-sm label-icon label-danger md-skip">
-                                                            <i class="fa fa-bolt"></i>
-                                                        </span> Storage server failed. </span>
-                                            <span class="time">9 days</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- END GROUP NOTIFICATION -->
                     <!-- BEGIN USER PROFILE -->
                     <div class="btn-group-img btn-group">
                         <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
@@ -1405,23 +1286,6 @@ License: You must have a valid license purchased only from themeforest(the above
                                                     </div>
                                                 </form>
                                             </div>
-                                            <li class="nav-item dropdown">
-                                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                    {{ "LOGOUT" }} <span class="caret"></span>
-                                                </a>
-
-                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                        {{ __('Logout') }}
-                                                    </a>
-
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                        @csrf
-                                                    </form>
-                                                </div>
-                                            </li>
                                             <!-- END PERSONAL INFO TAB -->
                                             <!-- CHANGE AVATAR TAB -->
                                             <div class="tab-pane" id="tab_1_2">
@@ -2205,3 +2069,4 @@ License: You must have a valid license purchased only from themeforest(the above
 </body>
 
 </html>
+@endsection
