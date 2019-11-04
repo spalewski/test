d33b066eed8c839c27583a74cd6c913e12bf8fbf
SebastianPal <spalewski@gmail.com>
2019-11-04 11:35:58 +0100

    Tables created: transactions, customers

---
+++ database/migrations/2019_11_04_101052_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('transactions')) {
+            Schema::create('transactions', function (Blueprint $table) {
+
+                $table->string('customer_id');
+                $table->foreign('customer_id')->references('customer_id')->on('customers')->onDelete('cascade');;
+                $table->integer('transaction_value');
+                $table->string('transaction_code');
+                $table->string('notes');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
